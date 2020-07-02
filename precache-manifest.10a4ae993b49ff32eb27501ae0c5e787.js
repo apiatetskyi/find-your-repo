8277c05271428f0f882550327994295abf62fb6f
a.piateteskyi <a.piatetskyi@gmail.com>
2020-07-02 20:13:50 +0300

    Updates

---
+++ precache-manifest.10a4ae993b49ff32eb27501ae0c5e787.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff5c57461a83bbdb686fc3a6aafb82b9",
+    "url": "/find-your-repo/index.html"
+  },
+  {
+    "revision": "1cf64bec2754ef64630d",
+    "url": "/find-your-repo/static/js/2.43c814c0.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/find-your-repo/static/js/2.43c814c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6fcc89a65bfefe91e9eb",
+    "url": "/find-your-repo/static/js/main.d7d37baa.chunk.js"
+  },
+  {
+    "revision": "ab47f8d3ecdd4e5c1763",
+    "url": "/find-your-repo/static/js/runtime-main.625fa1de.js"
+  }
+]);
