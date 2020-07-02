af29d8d550a3669e5c9faf6b0f1c6a0456bc8b3f
a.piateteskyi <a.piatetskyi@gmail.com>
2020-07-02 20:06:26 +0300

    add serviceworker registration

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import * as serviceWorker from "./serviceWorker";
 
 import App from "./components/App";
+
+serviceWorker.register();
 
 ReactDOM.render(<App />, document.querySelector("#root"));
